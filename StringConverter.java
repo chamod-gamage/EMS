7a5acba081ee25447adf1ebf5d85663add717c51
Spanishquisition <44648309+Spanishquisition@users.noreply.github.com>
2019-04-23 10:00:45 -0400

    Add files via upload

--- StringConverter.java
+++ StringConverter.java
@@ -8,9 +8,8 @@
  *
  * @author 618062
  */
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
 public final class StringConverter {
     
     private StringConverter(){}
@@ -28,18 +27,16 @@ public final class StringConverter {
         return true;
     }
     
+    public static String stripTrailingSpaces (String name) {
+        return name.trim();
+    }
+    
     public static List<Character> stringToChars (String text) {
-        
         List<Character> chars = new ArrayList<>();
-
         for (int i = 0; i < text.length(); i++) {
             chars.add(text.charAt(i));
 	}
         return chars;
-    }
-    
-    public static String stripTrailingSpaces (String name) {
-        return name.trim();
     }
     
     public static int stringToInteger(String employeeNumber) {
@@ -53,14 +50,59 @@ public final class StringConverter {
         return num;
     }
     
-    public static double stringToDouble (String doub) {
+    public static double stringToDollars (String doub) {
+        int perCounter = 0;
+        boolean hasSign = false;
         for (int x = 0; x < doub.length(); x++) {
             int ascii = doub.charAt(x);
             if ((ascii < 48 || ascii > 57) && ascii != 46) {
+                if (ascii == 36 && x == 0){
+                    hasSign = true;
+                    continue;
+                }
                 return -1;
             }
+            if (ascii == 46){
+                perCounter++;
+            }
+        }
+        if (perCounter > 1){
+            return -1;
+        }
+        if (hasSign){
+            String doubTwo = doub.substring(1);
+            double num = Double.parseDouble(doubTwo);
+            return num;
         }
         double num = Double.parseDouble(doub);
         return num;
     }
+    
+    public static double stringToPercent (String doub) {
+        int perCounter = 0;
+        boolean hasSign = false;
+        for (int x = 0; x < doub.length(); x++) {
+            int ascii = doub.charAt(x);
+            if ((ascii < 48 || ascii > 57) && ascii != 46) {
+                if (ascii == 37 && x == doub.length() - 1){
+                    hasSign = true;
+                    continue;
+                }
+                return -1;
+            }
+            if (ascii == 46){
+                perCounter++;
+            }
+        }
+        if (perCounter > 1){
+            return -1;
+        }
+        if (hasSign){
+            String doubTwo = doub.substring(0, doub.length() - 1);
+            double num = Double.parseDouble(doubTwo);
+            return num/100;
+        }
+        double num = Double.parseDouble(doub);
+        return num;
+    } 
 }
